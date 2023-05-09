7e7c23f7f29b2553cdc5e0b86e9ec7ec4f7bded5
Chris310005 <christopherus31.5@gmail.com>
2023-05-09 14:05:19 +0200

    Added new test cases for parsing

--- seraph/src/test/java/org/streamreasoning/gsp/Cypher_Parsing_Test.java
+++ seraph/src/test/java/org/streamreasoning/gsp/Cypher_Parsing_Test.java
@@ -52,6 +52,86 @@ public class Cypher_Parsing_Test {
     }
 
     @Test
+    public void query_test_mixed_arrows() throws IOException{
+
+        ContinuousQuery studentTrick = QueryFactory.parse("" +
+                "REGISTER QUERY student_trick STARTING AT 2022-10-14T14:45 {\n" +
+                "WITH duration({minutes : 5}) as _5m,\n" +
+                "duration({minutes : 20}) as _20m\n" +
+                "MATCH (s:Station)<-[r1:rentedAt]-(b1:Bike),\n" +
+                "(b1)-[n1:returnedAt]->(p:Station),\n" +
+                "(p)<-[r2:rentedAt]-(b2:Bike),\n" +
+                "(b2)-[n2:returnedAt]->(o:Station)\n" +
+                "WITHIN PT1H\n" +
+                "WHERE r1.user_id = n1.user_id AND\n" +
+                "n1.user_id = r2.user_id AND r2.user_id = n2.user_id AND\n" +
+                "n1.val_time < r2.val_time AND\n" +
+                "duration.between(n1.val_time,r2.val_time) < _5m AND\n" +
+                "duration.between(r1.val_time,n1.val_time) < _20m AND\n" +
+                "duration.between(r2.val_time,n2.val_time) < _20m\n" +
+                "EMIT r1.user_id, s.id, p.id, o.id\n" +
+                "ON ENTERING\n" +
+                "EVERY PT5M\n" +
+                "}");
+
+        String expected_query = "" +
+                "WITH duration({minutes : 5}) as _5m,\n" +
+                "duration({minutes : 20}) as _20m\n" +
+                "MATCH (s:Station)<-[r1:rentedAt]-(b1:Bike),\n" +
+                "(b1)-[n1:returnedAt]->(p:Station),\n" +
+                "(p)<-[r2:rentedAt]-(b2:Bike),\n" +
+                "(b2)-[n2:returnedAt]->(o:Station)\n" +
+                "WHERE r1.user_id = n1.user_id AND\n" +
+                "n1.user_id = r2.user_id AND r2.user_id = n2.user_id AND\n" +
+                "n1.val_time < r2.val_time AND\n" +
+                "duration.between(n1.val_time,r2.val_time) < _5m AND\n" +
+                "duration.between(r1.val_time,n1.val_time) < _20m AND\n" +
+                "duration.between(r2.val_time,n2.val_time) < _20m\n" +
+                "EMIT r1.user_id, s.id, p.id, o.id\n";
+
+        assertEquals(expected_query, studentTrick.getR2R());
+
+    }
+
+    @Test
+    public void query_test_or_operator() throws IOException{
+
+        ContinuousQuery studentTrick = QueryFactory.parse("" +
+                "REGISTER QUERY student_trick STARTING AT 2022-10-14T14:45 {\n" +
+                "WITH duration({minutes : 5}) as _5m,\n" +
+                "duration({minutes : 20}) as _20m\n" +
+                "MATCH (s:Station)->[r1:rentedAt]-(b1:Bike|Bike_test),\n" +
+                "(b1)-[n1:returnedAt]->(p:Station)\n" +
+                "WITHIN PT1H\n" +
+                "WHERE r1.user_id = n1.user_id AND\n" +
+                "n1.user_id = r2.user_id AND r2.user_id = n2.user_id AND\n" +
+                "n1.val_time < r2.val_time AND\n" +
+                "duration.between(n1.val_time,r2.val_time) < _5m AND\n" +
+                "duration.between(r1.val_time,n1.val_time) < _20m AND\n" +
+                "duration.between(r2.val_time,n2.val_time) < _20m\n" +
+                "EMIT r1.user_id, s.id, p.id, o.id\n" +
+                "ON ENTERING\n" +
+                "EVERY PT5M\n" +
+                "}");
+
+        String expected_query = "" +
+                "WITH duration({minutes : 5}) as _5m,\n" +
+                "duration({minutes : 20}) as _20m\n" +
+                "MATCH (s:Station)->[r1:rentedAt]-(b1:Bike|Bike_test),\n" +
+                "(b1)-[n1:returnedAt]->(p:Station)\n" +
+                "WHERE r1.user_id = n1.user_id AND\n" +
+                "n1.user_id = r2.user_id AND r2.user_id = n2.user_id AND\n" +
+                "n1.val_time < r2.val_time AND\n" +
+                "duration.between(n1.val_time,r2.val_time) < _5m AND\n" +
+                "duration.between(r1.val_time,n1.val_time) < _20m AND\n" +
+                "duration.between(r2.val_time,n2.val_time) < _20m\n" +
+                "EMIT r1.user_id, s.id, p.id, o.id\n";
+
+        assertEquals(expected_query, studentTrick.getR2R());
+
+    }
+
+    @Test
     public void query_test_studentTrick() throws IOException{
 
         ContinuousQuery studentTrick = QueryFactory.parse("" +
